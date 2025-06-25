a393d7080f8ac162da7ab39dd99a05d7549c58c2
uma-oo <oumayma.elfahsi99@gmail.com>
2025-06-25 18:55:45 +0100

    create the script to be run for installing the go-migrate

--- backend/database/sqlite/sqlite.go
+++ backend/database/sqlite/sqlite.go
@@ -1,2 +1,44 @@
 package sqlite 
 
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
+// the struct that will hold the connection !!
+
+type Database struct {
+	Database *sql.DB
+}
+
+func InitDB(nameDB string) (*Database, error) {
+	db, err := sql.Open("sqlite3", fmt.Sprintf("file:%v?_foreign_keys=1", nameDB))
+	if err != nil {
+		return nil, err
+	}
+	return &Database{db}, nil
+}
+
+func (db *Database) ReadSQL(filename string) error {
+	schema, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	statements := strings.Split(string(schema), ";")
+	for _, st := range statements {
+		_, err := db.Database.Exec(st + ";")
+		if err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
