0912f959e2d46f832f9fbbbe7f96e26a6f66692f
darkmethoss <ayoubstudies01@gmail.com>
2025-06-26 11:59:05 +0100

    saving updates

--- frontend/app/(main)/_components/navigation.js
+++ frontend/app/(main)/_components/navigation.js
@@ -1,23 +1,45 @@
+"use client"
 import Button from '@/app/_components/button'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import "./components.css"
 
 export default function Navigation() {
+  const currentPath = usePathname()
+  const routes = [
+    {
+      page: "Home",
+      href: "/"
+    },
+    {
+      page: "Profile",
+      href: "/profile/1"
+    },
+    {
+      page: "Groups",
+      href: "/groups"
+    },
+    {
+      page: "Chat",
+      href: "/chat"
+    }
+  ]
+
   return (
     <aside>
-      <h2>Social Network</h2>
-
-      <nav className={""}>
-        <Link href="/"><span>Home</span></Link>
-        <Link href="/profile"><span>Profile</span></Link>
-        <Link href="/groups"><span>Groups</span></Link>
-        <Link href="/chat"><span>Chat</span></Link>
+      <Link href={"/"}>
+        <h2>Social Network</h2>
+      </Link>
+      <nav>
+        {
+          routes.map(route => <Link className={`link ${route.href === currentPath ? "link-active" : ""}`} key={route.page} href={route.href}>{route.page}</Link>)
+        }
       </nav>
-      <Button variant='btn-primary'>
+      <Button variant='btn-danger'>
         <span>
           Log-out
         </span>
       </Button>
-
     </aside>
   )
 }
