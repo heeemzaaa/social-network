f7619b1a80b06850dc8e184dee66250910ddc0f9
heeemzaaa <hamzaelkhawlani00@gmail.com>
2025-06-26 19:14:27 +0100

    profile part in home done

--- frontend/app/(main)/_components/user_info.js
+++ frontend/app/(main)/_components/user_info.js
@@ -1,16 +1,35 @@
 import "./components.css"
 
-export default function InfosDiv({firstName, lastName, email, dateOfBirth, nickname}) {
+export default function InfosDiv({ firstName, lastName, email, dateOfBirth, nickname, followers, following, posts, groups }) {
   return (
-    <div className="ProfileContainer p2" style={{backgroundImage:`url(https://www.portraitprofessionnel.fr/wp-content/uploads/2020/02/portrait-professionnel-corporate-4.jpg)`}} >
-        {/* <img className="ProfileImg"></img> */}
-        <div className="ProfileData">
-              <span></span>First Name: {firstName}
-              Last Name: {lastName}
-              Email: {email}
-              Date of Birth: {dateOfBirth}
-              Nickname: {nickname}
+    <section className="profileLeftSection h-full">
+      <div className="ProfileContainer p2" style={{ backgroundImage: `url(https://www.portraitprofessionnel.fr/wp-content/uploads/2020/02/portrait-professionnel-corporate-4.jpg)` }} >
+        <div className="ProfileData p2 flex-col gap-1">
+          <p><span className="font-bold">First Name:</span> {firstName}</p>
+          <p><span className="font-bold">Last Name:</span> {lastName}</p>
+          <p><span className="font-bold">Email:</span> {email}</p>
+          <p><span className="font-bold">Date of Birth:</span> {dateOfBirth}</p>
+          <p><span className="font-bold">Nickname:</span> {nickname}</p>
         </div>
-    </div>
+      </div>
+
+      <div className="UserFollowers p2">
+        <div className="followers p1">
+          <p className="font-bold">Followers</p><p>{followers}</p>
+        </div>
+
+        <div className="following p1">
+          <p className="font-bold">Following</p><p>{following}</p>
+        </div>
+
+        <div className="posts p1">
+          <p className="font-bold">Posts</p><p>{posts}</p>
+        </div>
+
+        <div className="groups p1">
+          <p className="font-bold">Groups</p><p>{groups}</p>
+        </div>
+      </div>
+    </section>
   )
 }
