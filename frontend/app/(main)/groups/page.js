33c1d8fc7139dc005b7b43085ff29ca86ff0daf4
uma-oo <oumayma.elfahsi99@gmail.com>
2025-06-29 16:04:14 +0100

    add some edits

--- frontend/app/(main)/groups/page.js
+++ frontend/app/(main)/groups/page.js
@@ -1,29 +1,50 @@
+'use client'
 
-
+import Button from '@/app/_components/button'
+import { HiOutlinePlus } from "react-icons/hi";
+import { CreateGroup } from "./_components/create_group"
+import { useState, useEffect, useRef } from 'react';
 
 
 export default function Groups() {
+  const [menu, showMenu] = useState(false)
+
+
+
+  function HandleShowMenu() {
+    console.log(menu, "heere");
+    showMenu(!menu)
+  }
+
+   const myElementRef = useRef(null);
+
+      useEffect(() => {
+        const element = myElementRef.current; 
+        console.log("heere inside the useeffect", element);
+       
+
+        if (element) {
+          const handleClick = () => {
+            HandleShowMenu()
+          };
+
+          element.addEventListener('click', handleClick);
+          return () => {
+            element.removeEventListener('click', handleClick);
+          };
+        }
+      }, []); 
   return (
-    <div>Groups</div>
+    <main >
+      <Button className="align-center" onClick={HandleShowMenu}>
+        <HiOutlinePlus />create group</Button>
+      {menu && <CreateGroup ref={myElementRef} />}
+
+
+    </main>
   )
 }
 
 
 
 
-function OtherGroups(){
-
-}
-
-
-function  CreateGroup(){
-
-}
-
-function  JoinGroup(){
-
-}
-
-function  DisplayGroup() {
-
-}
