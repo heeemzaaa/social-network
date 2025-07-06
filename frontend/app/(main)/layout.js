43d8df05717283ba9d271f6566339e4ec66ced2a
DarkMethoss <ayoubstudies01@gmail.com>
2025-07-06 13:29:59 +0100

    save changes

--- frontend/app/(main)/layout.js
+++ frontend/app/(main)/layout.js
@@ -1,12 +1,5 @@
-import { useEffect, useState } from 'react'
 import Header from './_components/header'
 import Navigation from './_components/navigation'
-
-
-let notification = {
-  type: "",
-  message: "",
-}
 
 export default function MainLayout({ children }) {
   return (
