f732a44077289fdbcf625fadfb9076d1f273fa94
uma-oo <oumayma.elfahsi99@gmail.com>
2025-07-07 18:20:34 +0100

    add the code that needs to be edited because of all the hardcoded shit

--- backend/repositories/group/repo_group.go
+++ backend/repositories/group/repo_group.go
@@ -36,16 +36,45 @@ func (repo *GroupRepository) JoinGroup(group *models.Group, userId string) *mode
 	return nil
 }
 
-
-
-//  group title and description and up to 20 memebers joined the group and the number of the members
+//	group title and description and up to 20 memebers joined the group and the number of the members
+//
 // the number of posts created !!
 func (repo *GroupRepository) GetGroupDetails(groupId string) (*models.Group, *models.ErrorJson) {
 	var groupDetails *models.Group
+	query := `
+	WITH
+    cte_members AS (
+        SELECT
+            concat (users.firstName, " ", users.lastName) AS FullName,
+            group_membership.groupID AS Id,
+            count(group_membership.groupID) AS Nbr_Members
+        FROM
+            users
+            INNER JOIN group_membership ON users.userID = group_membership.userID
+        GROUP BY
+            Id
+    )
+	SELECT
+		groups.title,
+		groups.description,
+		groups.imagePath,
+		cte_members.FullName,
+		cte_members.Nbr_Members
+	FROM
+		groups
+		INNER JOIN cte_members ON groups.groupID = cte_members.Id
+		AND groups.groupID = "f90492c4-a062-4160-a934-7c06c12c4499"
+	
+	`
 
-     
-
-
-
+	stmt, err := repo.db.Prepare(query)
+	if err != nil {
+		return nil, &models.ErrorJson{Status: 500, Message: fmt.Sprintf("%v 4", err)}
+	}
+	defer stmt.Close()
+	_, err = stmt.Query()
+	if err != nil {
+		return nil, &models.ErrorJson{Status: 500, Message: fmt.Sprintf("%v 4", err)}
+	}
 	return groupDetails, nil
 }
