d696315f4d72cef61ab49a290e0636bd24210999
DarkMethoss <ayoubstudies01@gmail.com>
2025-07-12 13:55:14 +0100

    implement modal component with context provider and styling

---
+++ frontend/app/(main)/_context/ModalContext.js
@@ -0,0 +1,31 @@
+// app/_context/ModalContext.jsx or .tsx
+'use client';
+
+import React, { createContext, useContext, useState } from 'react';
+import Modal from '../_components/modal';
+
+export const ModalContext = createContext();
+export const useModal = () => useContext(ModalContext);
+
+export function ModalProvider({ children }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+
+    const openModal = (content) => {
+        console.log("opening modal: ", content)
+        setIsModalOpen(true);
+        setModalContent(content);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setModalContent(null);
+    };
+
+    return (
+        <ModalContext.Provider value={{ openModal, closeModal }}>
+            {children}
+            <Modal isModalOpen={isModalOpen} modalContent={modalContent} onClose={closeModal} />
+        </ModalContext.Provider>
+    );
+}
