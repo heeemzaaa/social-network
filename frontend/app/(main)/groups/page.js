7cd4e666a6a33c527c7d0bf20f768ad0c8aa1a6d
DarkMethoss <ayoubstudies01@gmail.com>
2025-07-12 18:26:04 +0100

    feat: integrate modal functionality for creating posts and groups in header and groups components

--- frontend/app/(main)/groups/page.js
+++ frontend/app/(main)/groups/page.js
@@ -1,61 +1,26 @@
 'use client'
 
 import Button from '@/app/_components/button'
-import { HiOutlinePlus } from "react-icons/hi";
-import styles from  "./group.module.css"
 import { CreateGroup } from "./_components/createGroup"
-import  {CardList, myItems} from "./_components/cardLists"
-import { useState, useEffect, useRef } from 'react';
+import Tabs from '../_components/tab/tabs';
+import Tab from '../_components/tab/tab';
+import TabContent from '../_components/tab/tabContent';
+import { useModal } from '../_context/ModalContext';
 
 
 export default function Groups() {
-  const [menu, showMenu] = useState(false)
-
-
-
-  function HandleShowMenu() {
-    console.log(menu, "before");
-    showMenu(!menu)
-    console.log(menu, "after");
-  }
-
-  // const myElementRef = useRef(null);
-
-  // useEffect(() => {
-  //   const element = myElementRef.current;
-  //   console.log("hnaaaaaa", myElementRef.current);
-  //   if (element) {
-  //     const handleClick = () => {
-  //       HandleShowMenu()
-  //     };
-
-  //     element.addEventListener('click', handleClick);
-  //     return () => {
-  //       element.removeEventListener('click', handleClick);
-  //     };
-  //   }
-  // }, []);
+  const { openModal } = useModal()
   return (
-    <main>
-      <div className={`${styles.container_1}`}>
-        <CardList  title={"My groups"} items={myItems} />
-      </div>
-      <div className={`${styles.container_2}`}>
-        <div className={`${styles.create_bouton}`}>
-          <Button className="justify-end" onClick={HandleShowMenu}>
-            <HiOutlinePlus />
-            create group
-          </Button>
-          {menu && <CreateGroup />}
-        </div>
-        <div className={`${styles.groups_unjoined}`}>
-          
-        </div>
-      </div>
+    <main className='flex-col '>
+      <Button className={'self-end'} onClick={() => { openModal(<CreateGroup />) }}>Add Post</Button>
+      <Tabs className={'flex-grow'}>
+        <Tab label="Your Groups" />
+        <Tab label="Joined Groups" />
+        <Tab label="Groups" />
+        <TabContent>Content for Tab 1</TabContent>
+        <TabContent>Content for Tab 2</TabContent>
+        <TabContent>Content for Tab 3</TabContent>
+      </Tabs>
     </main>
   )
 }
-
-
-
-
