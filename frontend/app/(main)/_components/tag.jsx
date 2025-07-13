721a6cd6ceaf91e256eece300f4b681a8878faef
DarkMethoss <ayoubstudies01@gmail.com>
2025-07-13 15:30:05 +0100

    feat: implement forms for adding groups and events, enhance group card display, and improve tab functionality

---
+++ frontend/app/(main)/_components/tag.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Tag({ children, className}) {
+    return (
+        <div className={`tag ${className}`} style={style}>
+            {children}
+        </div>
+    )
+}
+
+const style = {
+    background : "#ddd",
+    fontSize : "1rem",
+    color: "black",
+    width : "max-content",
+    padding : ".2rem .5rem",
+    display : "flex",
+    alignItems :"center",
+    gap : ".2rem",
+    borderRadius : ".4rem"
+}
